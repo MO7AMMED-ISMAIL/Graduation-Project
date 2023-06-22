c54f14145498a269930d276541ac6207bf681546
MO7AMMED-ISMAIL <mo7medismail200@gmail.com>
2023-06-22 20:53:55 +0300

    face_reco

---
+++ sender/sender.ino
@@ -0,0 +1,26 @@
+#include <Keypad.h>
+
+const byte ROWS = 4; 
+const byte COLS = 4;
+char keys[ROWS][COLS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+byte rowPins[ROWS] = {4, 5, 6, 7}; 
+byte colPins[COLS] = {8, 9, 10, 11};
+
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+void setup(){
+  Serial.begin(1200);
+}
+  
+void loop(){
+  char key = keypad.getKey();
+  if (key){
+    Serial.print(key);
+    delay(100);
+  }
+}
